0de60a2d54019c1bf16d3fb20d0d70b978cc35cd
markiep <purcellmarkie@gmail.com>
2022-10-26 17:56:46 -0700

    update

--- main.py
+++ main.py
@@ -2,14 +2,29 @@
 import cadquery as cq
 from params.default import *
 
+#result1 = (
+#    cq.Workplane("front")
+#    .box(2, 2, 0.5)
+#    .faces(">Z")
+#    .workplane()
+#    .rect(1.5, 1.5, forConstruction=True)
+#    .vertices()
+#    .hole(0.125)
+#)
+
 result = (
     cq.Workplane("front")
-    .box(2, 2, 0.5)
-    .faces(">Z")
-    .workplane()
-    .rect(1.5, 1.5, forConstruction=True)
-    .vertices()
-    .hole(0.125)
+    .hLine(base_diameter/2)
+    .vLine(base_height)
+    .hLineTo(inset_diameter/2)
+    .vLine(inset_height)
+    .lineTo(diameter/2, base_height+inset_height+5)
+    .lineTo(tip_radius, length-tip_radius)
+    .radiusArc((0, length), -tip_radius)
+    .close()
 )
 
+result = result.revolve()
+
 cq.exporters.export(result, file_name, file_type)
+
--- params/default.py
+++ params/default.py
@@ -1,16 +1,33 @@
 # the default parameters for the generator
-# mm are the base unit
-# TODO: add json support
+# unit = mm
+import cadquery as cq
 
 # define export params
 file_name = "models/butt_plug.step"
 file_type = "STEP"
 
 length = 5 * 25.4
-# this is the base diameter
-diameter = 3 * 25.4
 
-tip_radius = .1 * 25.4
+# this is the diameter at the base
+base_diameter = 3 * 25.4
+# height of the base
+base_height = 10
+
+# this is the gape diameter
+diameter = base_diameter
+
+# between 0 and the diameter
+inset_diameter = diameter / 2
+inset_height = .8 * 25.4
+
+# outer arc height above the inset
+outer_arc_height = .3 * 25.4
+
+# 0 turns it to a strait line
+outer_arc = 0
+inner_arc = 0
+
+tip_radius = .2 * 25.4
 
 threaded_hole_id = 0
 
